7b8e6b9ef0520ceb98259ced38fe8bd87d392f07
MateusGildev <mateuss.gill@hotmail.com>
2023-11-14 18:36:10 -0300

    Project created

---
+++ src/test/java/br/com/dsList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
